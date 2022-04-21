af5f69312aacf395a1b536e368633b5bfa36821d
Mykhailo Syritchenko <syritchenko@gmail.com>
2022-04-21 23:42:10 +0200

    Modified ../App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import './App.css';
-import Footer from './components/Footer';
-import Home from './pages/Home';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
 
 function App() {
 	return (
-		<div className="App">
+		<>
 			<Home />
 			<Footer />
-		</div>
+		</>
 	);
 }
 
