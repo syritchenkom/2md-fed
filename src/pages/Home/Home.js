0cb840aef32a88aee0b7eabc1bcbf4c7912d236f
Mykhailo Syritchenko <syritchenko@gmail.com>
2022-04-21 23:50:42 +0200

    Refreshed ../pages/Home/

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import CardTable from '../../components/CardTable/CardTable';
+import axios from 'axios';
+import './Home.scss';
+
+function Home() {
+	const [galleryModals, setGalleryModals] = useState([]);
+	useEffect(() => {
+		axios.get('https://picsum.photos/v2/list?limit=10').then((res) => {
+			setGalleryModals(
+				res.data.map((selectedImg) => {
+					return selectedImg;
+				})
+			);
+		});
+	}, []);
+	// Loading message for array
+	if (!galleryModals?.length) {
+		return <h5>Loading...</h5>;
+	}
+	return (
+		<Container className="homeContainer">
+			<CardTable
+				galleryModals={galleryModals}
+				setGalleryModals={setGalleryModals}
+			/>
+		</Container>
+	);
+}
+
+export default Home;
