cc68356b757dd810a1b74c6b349e422df71583cb
Mykhailo Syritchenko <syritchenko@gmail.com>
2022-04-21 23:49:08 +0200

    Refreshed ../components/Carousel/

---
+++ src/components/Carousel/CarouselItem.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+import './CarouselItem.scss';
+
+const CarouselItem = ({
+	modalOpen,
+	setModalOpen,
+	galleryModals,
+	setGalleryModals
+}) => {
+	// const [index, setIndex] = useState(false);
+	const [index, setIndex] = useState(0);
+
+	const handleSelect = (selectedIndex, e) => {
+		setIndex(selectedIndex);
+	};
+
+	return (
+		<>
+			<Carousel
+				className="Carousel"
+				prevIcon={
+					<span aria-hidden="true" className="carousel-control-prev-icon">
+						<i class="bi bi-chevron-left"></i>
+					</span>
+				}
+				nextIcon={
+					<span aria-hidden="true" className="carousel-control-next-icon">
+						<i class="bi bi-chevron-right"></i>
+					</span>
+				}
+				indicators={false}
+				interval={null}
+				// autoPlay={false}
+				activeIndex={index}
+				onSelect={handleSelect}>
+				{/* Take data-photo from picsum.photos*/}
+				{galleryModals.map((image, index) => {
+					const { author, download_url } = image;
+					return (
+						<Carousel.Item key={index} className="carouselItem">
+							<img
+								className="d-block w-100 h-100"
+								src={download_url}
+								alt={author}
+							/>
+							<Carousel.Caption className="carouselCaption">
+								Photo's {author}
+							</Carousel.Caption>
+						</Carousel.Item>
+					);
+				})}
+			</Carousel>
+		</>
+	);
+};
+
+export default CarouselItem;
